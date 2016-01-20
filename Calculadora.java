eca7c0840f31a3ab446816a3f69c862a66278282
dyego1912 <dyego1607@gmail.com>
2016-01-20 14:06:37 -0200

    Atualização da Calculadora
    Implementação da subtração

--- Calculadora.java
+++ Calculadora.java
@@ -2,9 +2,14 @@ public Class Calculadora{
   public static int somaDoisNumeros(int a, int b){
     return a + b;
   }
+  
+  public static int subtracao(int a, int b){
+    return a-b
+  }
   public static void main(String [] args){
   int a,b;
-  System.out.println(a+b);
+  System.out.println("Soma: " a+b);
+  System.out.println("Subtração: " a-b);
   
   }
 }  
